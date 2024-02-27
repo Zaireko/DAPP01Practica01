44540f6d0e430f7f2b562f2ea27bb0226427febd
Yadhir Teoba <yadhirTV@gmail.com>
2024-02-27 15:44:57 -0600

    "Integración de Hibernate"

--- src/main/java/org/uv/dapp01practica01/DAPP01Practica01.java
+++ src/main/java/org/uv/dapp01practica01/DAPP01Practica01.java
@@ -4,12 +4,28 @@
 
 package org.uv.dapp01practica01;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author zaireko
  */
 public class DAPP01Practica01 {
     public static void main(String[] args) {
-        Menus.menu();
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        PojoEmpleado empleado = new PojoEmpleado();
+        empleado.setNombre("Galicia Hernandez Hernandez");
+        empleado.setDireccion("Calle 89");
+        empleado.setTelefono("1234567890");
+        
+        Session session = sf.getCurrentSession();
+        Transaction transacition = session.beginTransaction();
+        session.save(empleado);
+        transacition.commit();
+        System.out.println("Guardao' con id " + empleado.getId());
+        
+        //Menus.menu();
     }
 }
