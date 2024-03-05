f14498b43cc626671924a331434964b507daf014
Yadhir Teoba <yadhirTV@gmail.com>
2024-03-04 19:34:12 -0600

    Hibernate de ventas completo

--- src/main/java/org/uv/dapp01practica01/DAOVenta.java
+++ src/main/java/org/uv/dapp01practica01/DAOVenta.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.uv.dapp01practica01;
 
 import java.util.List;
@@ -12,97 +8,115 @@ import org.hibernate.query.Query;
 
 /**
  *
- * @author zaireko
+ * @author yodoeaoffi06
  */
 public class DAOVenta implements IDAOGeneral<Venta, Long> {
 
     @Override
     public Venta guardar(Venta pojo) {
-        try (SessionFactory sf = HibernateUtil.getSessionFactory();
-             Session session = sf.getCurrentSession()) {
-            Transaction transaction = session.beginTransaction();
-            
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
             session.save(pojo);
             for (DetalleVenta det : pojo.getDetalleVentas()) {
                 session.save(det);
             }
-            
+
             transaction.commit();
             return pojo;
         } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             e.printStackTrace();
             return null;
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public Venta modificar(Venta pojo, Long clave) {
-        try (SessionFactory sf = HibernateUtil.getSessionFactory();
-             Session session = sf.getCurrentSession()) {
-            Transaction transaction = session.beginTransaction();
-            
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
             session.update(pojo);
             transaction.commit();
-            
             return pojo;
         } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             e.printStackTrace();
             return null;
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public boolean eliminar(Long clave) {
-        try (SessionFactory sf = HibernateUtil.getSessionFactory();
-             Session session = sf.getCurrentSession()) {
-            Transaction transaction = session.beginTransaction();
-            
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
             Venta venta = session.get(Venta.class, clave);
             if (venta != null) {
                 session.delete(venta);
                 transaction.commit();
                 return true;
+            } else {
+                transaction.rollback();
+                return false;
             }
-            
-            return false;
         } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             e.printStackTrace();
             return false;
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public Venta buscarById(Long id) {
-        try (SessionFactory sf = HibernateUtil.getSessionFactory();
-             Session session = sf.getCurrentSession()) {
-            Transaction transaction = session.beginTransaction();
-            
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
             Venta venta = session.get(Venta.class, id);
-            
-            transaction.commit();
-            
             return venta;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public List<Venta> buscarAll() {
-        try (SessionFactory sf = HibernateUtil.getSessionFactory();
-             Session session = sf.getCurrentSession()) {
-            Transaction transaction = session.beginTransaction();
-            
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
             Query<Venta> query = session.createQuery("FROM Venta", Venta.class);
             List<Venta> ventas = query.getResultList();
-            
-            transaction.commit();
-            
             return ventas;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        } finally {
+            session.close();
         }
     }
 }
